abadc7ef21a4645e7170f6f6b57e3ad8271fa415
Shawn Reuter <reute078@morris.umn.edu>
2022-03-25 12:54:28 -0500

    Add Canvas for StatUI
    Added many different text

---
+++ Assets/Scripts/StatUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatUI : MonoBehaviour
+{
+    
+    [SerializeField] private TMP_Text enemiesKilled;
+    [SerializeField] private TMP_Text waveNumber;
+
+    [SerializeField] private TMP_Text gold;
+    [SerializeField] private TMP_Text attackSpeed;
+    [SerializeField] private TMP_Text health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
