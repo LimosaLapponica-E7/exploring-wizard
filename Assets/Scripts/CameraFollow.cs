4b3ea85fa7a929a5a5a706afdaa138b12be0821b
Johannes Martinez <mart4695@morris.umn.edu>
2022-03-26 11:54:36 -0500

    Make camera follow player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform mainTransform;
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       Vector3 currentPos = mainTransform.position;
+       currentPos.x = target.position.x;
+       currentPos.y = target.position.y;
+       mainTransform.position = currentPos; 
+    }
+}
