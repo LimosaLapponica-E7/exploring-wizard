92ffe94ed0f436053443dbeb623a3cc3cfe8af26
Shawn Reuter <reute078@morris.umn.edu>
2022-03-28 12:58:15 -0500

    Add script PlayerStats
    Added script PlayerStats to start thinking about how we will keep track of the player's health and other things.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats playerStats;
+
+    public GameObject player;
+
+    public float health;
+    public float maxHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void dealDamage(float damage)
+    {
+        health -= damage;
+        CheckDeath();
+    }
+
+    public void healCharacter(float heal)
+    {
+        health += heal;
+        CheckOverheal();
+    }
+
+    private void CheckOverheal()
+    {
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    private void CheckDeath()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
