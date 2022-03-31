02d2ee87ec944f89bc74a4044aa5a4ae8c7537f4
Johannes Martinez <mart4695@morris.umn.edu>
2022-03-31 18:18:50 -0500

    Do not allow camera to follow a killed player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,19 +6,23 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform mainTransform;
     [SerializeField] private Transform target;
+    [SerializeField] private GameObject targetObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       Vector3 currentPos = mainTransform.position;
-       currentPos.x = target.position.x;
-       currentPos.y = target.position.y;
-       mainTransform.position = currentPos; 
+        if (targetObject.activeSelf)
+        {
+            Vector3 currentPos = mainTransform.position;
+            currentPos.x = target.position.x;
+            currentPos.y = target.position.y;
+            mainTransform.position = currentPos;
+        }
     }
 }
