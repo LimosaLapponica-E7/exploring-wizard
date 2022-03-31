94a2ecaf810035a1bbd9b66fcac02684df0082b5
Colt Dahl <dahlcolt@gmail.com>
2022-03-31 12:02:52 -0500

    Add timesinceflip to negate erratic flipping when you are constantly crossing the x barrier

--- Assets/Scripts/Enemy/EnemyMovementTowardPlayer.cs
+++ Assets/Scripts/Enemy/EnemyMovementTowardPlayer.cs
@@ -12,6 +12,7 @@ public class EnemyMovementTowardPlayer : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     private SpriteRenderer spriteRenderer;
+    private float timesinceflip = 0.0f;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -20,17 +21,20 @@ public class EnemyMovementTowardPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timesinceflip += Time.deltaTime;
         spriteRenderer = this.GetComponent<SpriteRenderer>();
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan(direction.x) * Mathf.Rad2Deg;
         movement = direction;
-        if(direction.x < 0)
+        if(direction.x < 0 && timesinceflip > 0.33)
         {
             spriteRenderer.flipX = true;
+            timesinceflip = 0;
         }
-        else
+        if(direction.x > 0 && timesinceflip > 0.33)
         {
             spriteRenderer.flipX = false;
+            timesinceflip = 0;
         }
     }
 
