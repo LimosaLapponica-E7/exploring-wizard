5bf5db3b9eb9a204dd821e6f5078244cbd363937
Colt Dahl <dahl1173@morris.umn.edu>
2022-04-08 13:00:16 -0500

    Add alpha coloring to bird sprite, add flockbehavior, flockagent in prep for bird flocking enemy system

--- Assets/Scripts/Enemy/FlockingManager.cs
+++ Assets/Scripts/Enemy/FlockingManager.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class FlockingManager : MonoBehaviour
 {
-    [SerializeField] public GameObject flockUnit;
+    [SerializeField] public FlockAgent flockUnit;
     [SerializeField] public int numUnits;
     public GameObject[] allUnits;
     public Vector2 flockLimits = new Vector2(5, 5);
 
+    const float flockDensity = 0.08f;
+    public float driveFactor = 10f;
+    public float neighborRadius = 0.5f;
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float squareMaxSpeed;
+    float squareNeighborRadius;
+    public float SquareAvoidanceRadius { get { return SquareAvoidanceRadius; } }
     // Start is called before the first frame update
     void Start()
     {
+        squareMaxSpeed = squareMaxSpeed * squareMaxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        SquareAvoidanceRadiusRadius = squareNeighborRadius * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < numUnits; i++)
+        {
+            Instantiate(
+                flockUnit,
+                Random.insideUnitCircle * startingCount * flockDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f),
+                transform);
+            newAgent.name = "flockUnit " + i;
+            agents.
+        }
+
         // allUnits = new GameObject[numUnits];
         // for(int i = 0; i < numUnits; i++)
         // {
