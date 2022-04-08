88344340ad1901f4369668fe0d6cce5440055d63
Johannes Martinez <mart4695@morris.umn.edu>
2022-04-08 12:27:55 -0500

    Add comments to ObstacleGenerator.cs

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -18,13 +18,21 @@ public class ObstacleGenerator : MonoBehaviour
 
     void Start()
     {
-
+        // Get terrain dimensions
         float cornerX = terrain.bounds.size.x / 2;
         float cornerY = terrain.bounds.size.y / 2;
+
+        /* Compute the number of obstacles corresponding to a particular 
+           density. The current setting is 1% the terrain size in square units
+           times the obstacle density setting.
+        */
         int numberOfObstacles = (int) (terrain.bounds.size.x * 
             terrain.bounds.size.y * 0.01 * obstacleDensity);
+
+        // Stores obstacle positions to avoid excessive overlap
         Vector2[] obstacleMap = new Vector2[numberOfObstacles];
 
+        // Generates the obstacles
         for (int i = 0; i < numberOfObstacles; i++)
         {
             GameObject obstaclePrefab = obstacles[Random.Range(0, obstacles.Length)];
@@ -33,6 +41,7 @@ public class ObstacleGenerator : MonoBehaviour
             newObstacle.transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
         }
 
+        // Attempts to find positions that avoid excessive overlap
         Vector2 randomPos(float minDistance, int i)
         {
             int limitCount = 0;
@@ -43,6 +52,8 @@ public class ObstacleGenerator : MonoBehaviour
                 Random.Range(-cornerY, cornerY));
                 obstacleMap[i] = obstaclePos;
                 limitCount++;
+                // Code for debugging. Avoids infinite loops by giving up on
+                // the mimimum distance requirements after 2000 attempts per obstacle
                 if (limitCount > 20000) {
                     Debug.Log("Too many obstacles");
                     break;
@@ -54,6 +65,7 @@ public class ObstacleGenerator : MonoBehaviour
             return obstaclePos;
         }
 
+        // Finds whether or not a proposed position is too close to a used position
         bool spaceUsed(Vector2 obstaclePos, int end)
         {
             bool spaceUsed = false;
