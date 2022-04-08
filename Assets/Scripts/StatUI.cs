db965b7f04e0679257fc18066c329ee5d87548a5
Shawn Reuter <reute078@morris.umn.edu>
2022-04-07 20:57:14 -0500

    Add Player StatUI that upon death will display

--- Assets/Scripts/StatUI.cs
+++ Assets/Scripts/StatUI.cs
@@ -7,13 +7,43 @@ public class StatUI : MonoBehaviour
 {
 
 
-    [SerializeField] private TMP_Text waveNumber;
+    [SerializeField] private TMP_Text levelNumber;
     [SerializeField] private TMP_Text gold;
-    [SerializeField] private TMP_Text attackSpeed;
     [SerializeField] private TMP_Text enemiesKilled;
+    [SerializeField] private TMP_Text attackSpeed;
+
+    public GameObject restartButton;
+    public GameObject uponDeathUI;
+    public GameObject playerHealthBar;
+
+    void Start()
+    {
+        uponDeathUI.SetActive(false);
+    }
+    public void updateLevelNumber(float count)
+    {
+        levelNumber.text = "Level:" + count;
+    }
+
+    public void UpdateGoldNumber(float count)
+    {
+        gold.text = "Enemies Defeated: " + count;
+    }
 
     public void ShowEnemyDefeatCount(float count)
     {
         enemiesKilled.text = "Enemies Defeated: " + count;
+    }
+
+    public void UponPlayerDeathDisplayUI()
+    {
+        uponDeathUI.SetActive(true);
+        playerHealthBar.SetActive(false);
+        restartButton.SetActive(true);
+    }
+
+    public void restartScene()
+    {
+
     }
 }
