6b897b1583b2b82b4177ec7ed6e258456a0197ab
Johannes Martinez <mart4695@morris.umn.edu>
2022-04-20 16:37:22 -0500

    Fix camera smoothening

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,15 +14,15 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 velocity = Vector3.zero;
         if (targetObject.activeSelf)
         {
             Vector3 currentPos = mainTransform.position;
-            Vector3 smoothPos = Vector3.SmoothDamp(mainTransform.position, target.position, ref velocity, 5f);
-            currentPos.x = target.position.x;
-            currentPos.y = target.position.y;
+            Vector3 smoothPos = Vector3.SmoothDamp(mainTransform.position, target.position, ref velocity, 0.075f);
+            currentPos.x = smoothPos.x;
+            currentPos.y = smoothPos.y;
             mainTransform.position = currentPos;
         }
     }
