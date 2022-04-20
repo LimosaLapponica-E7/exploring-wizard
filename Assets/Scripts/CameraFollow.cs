203ec756b306dab800b93b22842b1654de98c72d
Johannes Martinez <mart4695@morris.umn.edu>
2022-04-20 15:38:37 -0500

    Smooth camera motion

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,6 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform mainTransform;
     [SerializeField] private Transform target;
     [SerializeField] private GameObject targetObject;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +14,13 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
+        Vector3 velocity = Vector3.zero;
         if (targetObject.activeSelf)
         {
             Vector3 currentPos = mainTransform.position;
+            Vector3 smoothPos = Vector3.SmoothDamp(mainTransform.position, target.position, ref velocity, 5f);
             currentPos.x = target.position.x;
             currentPos.y = target.position.y;
             mainTransform.position = currentPos;
